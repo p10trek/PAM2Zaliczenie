085c656bebd67d14a650a3861f7a66c505623e64
Maciej Karpiński <karpfly@gmail.com>
2020-03-27 11:32:00 +0100

    documentation update, readme update,  added password encryption function SHA512

--- Models/Users.cs
+++ Models/Users.cs
@@ -12,8 +12,31 @@ namespace PAM2Zaliczenie.Models
 
         public int Id { get; set; }
         public string Login { get; set; }
+
         public string Password { get; set; }
 
         public virtual ICollection<Tasks> Tasks { get; set; }
+
+        //password hash SHA512 
+        public string passwordSHA512(string inputPassword)
+        {
+            var bytes = System.Text.Encoding.UTF8.GetBytes(inputPassword);
+            using (var hash = System.Security.Cryptography.SHA512.Create())
+            {
+                var hashedInputBytes = hash.ComputeHash(bytes);
+
+                var hashedInputStringBuilder = new System.Text.StringBuilder(128);
+                foreach (var b in hashedInputBytes)
+                {
+                    hashedInputStringBuilder.Append(b.ToString("X2"));
+                }
+
+                return hashedInputStringBuilder.ToString();
+            }
+        }
     }
 }
+
+
+
+
