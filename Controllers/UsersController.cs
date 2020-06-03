f7bcd78d99b8cc35174b7c011bb041a56d25c96e
Maciej Karpiński <karpfly@gmail.com>
2020-06-03 11:02:29 +0200

    added self details and self edit

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
@@ -26,6 +27,7 @@ namespace PAM2Zaliczenie.Controllers
         }
 
         // GET: Users/Details/5
+        [Authorize(Roles = "0, 1")]
         public async Task<IActionResult> Details(Guid id)
         {
             if (id == null)
@@ -150,6 +152,83 @@ namespace PAM2Zaliczenie.Controllers
         private bool UsersExists(Guid id)
         {
             return _context.Users.Any(e => e.Id == id);
+        }
+
+        // GET: Users/SelfEdit/5
+        public async Task<IActionResult> SelfEdit(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.Users.FindAsync(id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+            return View(users);
+        }
+
+        // POST: Users/SelfEdit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> SelfEdit(Guid id, [Bind("Id,Login,EmailAddress,UserAccessLevel,Password")] Users users)
+        {
+            if (id != users.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                Debug.WriteLine("Model state valid");
+                try
+                {
+                    Debug.WriteLine("Przed zapisem do bazy");
+
+                    _context.Update(users);
+                    await _context.SaveChangesAsync();
+                    Debug.WriteLine("Zapisano do bazy");
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    Debug.WriteLine("DbUpdateConcurency");
+                    if (!UsersExists(users.Id))
+                    {
+                        Debug.WriteLine("!UserExists");
+                        return NotFound();
+                    }
+                    else
+                    {
+                        Debug.WriteLine("Else");
+                        throw;
+                    }
+                }
+                Debug.WriteLine("Redirect");
+                return RedirectToAction(nameof(SelfDetails), new { users.Id });
+            }
+            Debug.WriteLine("return user");
+            return View(users);
+        }
+        // GET: Users/SelfDetails/5
+        public async Task<IActionResult> SelfDetails(Guid id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var users = await _context.Users
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (users == null)
+            {
+                return NotFound();
+            }
+
+            return View(users);
         }
     }
 }
